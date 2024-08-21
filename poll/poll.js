e7ae16629f7421b46ebeb649025d484b7cdcbe7e
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2024-08-21 16:20:19 +0200

    Customization

--- poll/poll.js
+++ poll/poll.js
@@ -10,7 +10,7 @@ var Poll = (function(){
 
 var refresh_interval = null;
 var current_poll = null;
-var url = "https://fraage.de";
+var url = document.currentScript.src.split("/").slice(0,-1).join("/");
 
 function show_status() {
     $.get( url+"/api/?method=status", function( res ) { 
@@ -20,6 +20,17 @@ function show_status() {
 }
 
 function start_poll() {
+    console.log('start_poll');
+
+    $(".poll > ul > li").each(function(i) {
+        this.innerHTML = '<span class="poll-percentage"></span>'
+                        +'<span class="poll-answer-text">'+this.innerHTML+'</span>';
+    });
+
+    $(".poll").not(":has(>.poll-responses)").each(function(i) {
+        $(this).append('<span class="poll-responses"></span>');
+    });
+            
     var question = $(current_poll).children("h1").text();
 
     var answers = [];
@@ -38,8 +49,9 @@ function start_poll() {
     refresh_interval = window.setInterval(show_status, 1000);
 }
 
-function stop_poll() { 
-    if(current_poll == null) { return; }
+function stop_poll() {
+    console.log('stop_poll');
+    if(current_poll == null) { console.log('No current poll'); return; }
     clearInterval(refresh_interval);
     $(current_poll).find("ul > li > .poll-percentage").css("width","0%");
     $.get( url+"/api/?method=stop_poll", function( res ) { 
@@ -60,27 +72,25 @@ function stop_poll() {
 
 return {
     init: function() {    
-        if(window.location.search.match( /print-pdf/gi )) {
-            /* don't show poll in print view */
+        if(! window.location.search.match( /poll=yes/gi )) {
+            // Only show poll with query param ?poll=yes
+            // Note that if you insert polls as fragments,
+            // invisible fragments will remain.
             return;
         }
         Reveal.on('ready', () => {
-            $(".poll > ul > li").not(":has(>span)").click(function() {
+            $(".poll > ul > li").click(function(event) {
                 stop_poll();
+                event.stopPropagation();
             });
 
-            $(".poll > h2").html(url);
-
-            $(".poll > ul > li").not(":has(>span)").each(function(i) {
-                this.innerHTML = '<span class="poll-percentage"></span>'
-                                +'<span class="poll-answer-text">'+this.innerHTML+'</span>';
-            });
-
-            $(".poll").not(":has(>.poll-responses)").each(function(i) {
-                $(this).append('<span class="poll-responses"></span>');
-            });
+            $(".poll > h2").html("chezsoi.org/poll"); // Originally: $(".poll > h2").html(url);
 
             $(".poll").show();
+
+            if ($(".poll").hasClass("visible")) {
+                current_poll = $(".poll");
+            }
 
             Reveal.on('fragmentshown', (event) => {
                 if(!$(event.fragment).hasClass("poll")) { return; }
